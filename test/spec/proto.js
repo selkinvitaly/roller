8353b206b51c05f9b9a7f06f27f421e8671557c6
Selkin Vitaly <contact@selkinvitaly.ru>
2015-12-11 01:37:56 +0300

    test(proto.js): added test for the method _findRollingElements

--- test/spec/proto.js
+++ test/spec/proto.js
@@ -138,4 +138,50 @@
 
   });
 
+  // Roller.prototype._findRollingElements()
+  describe("Method _findRollingElements", function() {
+    let append, remove;
+
+    beforeAll(function() {
+
+      append = function() {
+        let args = Array.prototype.slice.apply(arguments);
+
+        args.forEach((elem) => document.body.appendChild(elem));
+      };
+
+      remove = function() {
+        let args = Array.prototype.slice.apply(arguments);
+
+        args.forEach((elem) => document.body.removeChild(elem));
+      };
+
+    });
+
+    it("finds elements in DOM", function() {
+      let elements = {};
+      let area     = document.createElement("div");
+      let link     = document.createElement("a");
+      let area2    = document.createElement("div");
+      let link2    = document.createElement("a");
+
+      area.setAttribute("data-roller-area", "area");
+      link.setAttribute("data-roller-link", "area");
+      area2.setAttribute("data-roller-area", "area2");
+      link2.setAttribute("data-roller-link", "area2");
+
+      append(area, area2, link, link2);
+
+      roller._findRollingElements(elements, "data-roller-area", "data-roller-link");
+
+      expect(elements.area.area).toBeDefined();
+      expect(elements.area.link).toBeDefined();
+      expect(elements.area2.area).toBeDefined();
+      expect(elements.area2.link).toBeDefined();
+
+      remove(area, area2, link, link2);
+    });
+
+  });
+
 }(window.Roller));
